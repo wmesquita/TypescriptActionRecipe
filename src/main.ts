32f7318a1208592243dbc6295e6dea13ccc80433
Walter Mesquita <walter.mesquita@gov.ab.ca>
2024-11-28 20:15:21 -0700

    typescript action

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,25 @@ export async function run(): Promise<void> {
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
+
+    // Write an advanded job summary
+    core.summary
+      .addHeading('Advanced Job Summary', 'h2')
+      .addImage('https://octodex.github.com/images/droitcat.png', 'Droitcat', {
+        width: '64',
+        height: '64'
+      })
+      .addTable([
+        [
+          { data: 'File', header: true },
+          { data: 'Result', header: true }
+        ],
+        ['foo.js', 'Pass :pass:'],
+        ['bar.js', 'Fail :fail:'],
+        ['test.js', 'Pass :pass:']
+      ])
+      .addLink('My custom link', 'https://writeabout.net')
+      .write()
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
